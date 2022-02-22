898bae50c4401f38f386b79880c0f33ad81acb22
iuliolteanu <iuli.olteanu.2000@gmail.com>
2022-02-22 18:42:00 +0200

    Am adaugat main+clase

--- src/sources/animals/Animal.java
+++ src/sources/animals/Animal.java
@@ -1,4 +1,20 @@
 package sources.animals;
 
 public abstract class Animal {
+    private String nume;
+
+    public Animal(String nume) {
+        super();
+        this.nume=nume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public abstract void mananca(String mancare);
 }
